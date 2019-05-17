a16e7eddabd48ece45856b85adf3e17b300efb12
oscar <mtz.castellanos72@gmail.com>
2019-05-17 09:43:22 -0500

    RestServer - Usuario

--- server/server.js
+++ server/server.js
@@ -1,42 +1,21 @@
 require('./config/config');
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
 var bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
-    //
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-});
-//Crear nuevos registros
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            persona: body
-        });
-    }
+app.use(bodyParser.json());
+//
+app.use(require('./rutas/usuario'));
 
-});
-//Actualizar datos
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-});
-//Eliminar datos
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
+mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true, useCreateIndex: true }, (err, resp) => {
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
