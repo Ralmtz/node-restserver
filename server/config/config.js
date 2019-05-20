342ea45752773098b275c143649b5a6202c3e8cc
oscar <mtz.castellanos72@gmail.com>
2019-05-20 11:49:59 -0500

    cambio en config.js

--- server/config/config.js
+++ server/config/config.js
@@ -5,10 +5,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb+srv://Ralmtz:ChzOVpaZbLCprwoI@cluster0-jikn6.mongodb.net/cafe?retryWrites=true';
-// }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://Ralmtz:ChzOVpaZbLCprwoI@cluster0-jikn6.mongodb.net/cafe?retryWrites=true';
+}
 
 process.env.URLDB = urlDB;
